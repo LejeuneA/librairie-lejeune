aa0f77bdb9d67ec9ff94381467742b435487f06a
Acelya Lejeune <145855362+LejeuneA@users.noreply.github.com>
2024-03-07 17:10:04 +0100

    install sass

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var includes = document.querySelectorAll('[data-include]');
+    
+    includes.forEach(function (element) {
+        var file = '../components/' + element.getAttribute('data-include') + '.html';
 
-// functions.js
-
-$(document).ready(function () {
-    var includes = $('[data-include]');
-    $.each(includes, function () {
-        var file = '/components/' + $(this).data('include') + '.html';
-        $(this).load(file);
+        // Fetch the HTML content
+        fetch(file)
+            .then(response => response.text())
+            .then(data => {
+                // Insert the HTML content into the element
+                element.innerHTML = data;
+            })
+            .catch(error => console.error('Error fetching ' + file, error));
     });
 });
-
-function toggleNavbarMenu() {
-    const offcanvasNavbar = new bootstrap.Offcanvas(document.getElementById('offcanvasNavbar'));
-    offcanvasNavbar.toggle();
-}
