cfec8e27456334aa419a08391acd8678d6ba81c9
A.Lejeune <acelyalejeune@gmail.com>
2024-04-19 11:09:33 +0200

    Update article.php

--- admin/article.php
+++ admin/article.php
@@ -5,10 +5,9 @@ $msg = null;
 $result = null;
 $execute = false;
 
-
 // Check if the ID of the livre is passed in the URL
-if (isset($_GET['idLivre']) && !empty($_GET['idLivre'])) {
-    $id = $_GET['idLivre']; // Retrieve the livre ID from the URL
+if (isset($_GET['id']) && !empty($_GET['id'])) {
+    $id = $_GET['id']; // Retrieve the livre ID from the URL
     // Ensure that the database connection object is valid
     if (!is_object($conn)) {
         $msg = getMessage($conn, 'error'); // Display an error message if the connection is not valid
@@ -23,62 +22,37 @@ if (isset($_GET['idLivre']) && !empty($_GET['idLivre'])) {
         }
     }
 } else {
-    $msg = getMessage('Il n\'y a pas d\'article à afficher', 'success'); // Display a success message if no livre ID is provided
+    $msg = getMessage('Il n\'y a pas d\'article à afficher', 'error'); // Display an error message if no livre ID is provided
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
-    <?php displayHeadSection((isset($result['title']) ? $result['title'] : APP_NAME)); ?>
+    <?php displayHeadSection((isset($result['titleLivre']) ? $result['titleLivre'] : APP_NAME)); ?>
 </head>
 
 <body>
-    <!-----------------------------------------------------------------
-							   Header
-	------------------------------------------------------------------>
     <header>
-        <!-----------------------------------------------------------------
-							   Navigation
-	    ------------------------------------------------------------------>
         <?php displayNavigation(); ?>
-        <!-----------------------------------------------------------------
-							Navigation end
-	    ------------------------------------------------------------------>
     </header>
-    <!-----------------------------------------------------------------
-							   Header end
-	------------------------------------------------------------------>
     <div class="container">
         <div id="message">
             <?php if (isset($msg)) echo $msg; ?>
         </div>
         <div id="content">
-
             <?php
             // Peut-on exécuter l'affichage de l'article
-            if ($execute)
+            if ($execute) {
                 displayLivreByID($result);
+            }
             ?>
-
         </div>
     </div>
-
-    <!-----------------------------------------------------------------
-								Footer
-	------------------------------------------------------------------>
     <footer>
         <div data-include="footer"></div>
     </footer>
-    <!-----------------------------------------------------------------
-							  Footer end
-	------------------------------------------------------------------>
-
-    <!-- Font Awesome JS -->
     <script src="https://kit.fontawesome.com/3546d47201.js" crossorigin="anonymous"></script>
-
-    <!-- Include functions.js -->
     <script src="../js/functions.js"></script>
 </body>
 
