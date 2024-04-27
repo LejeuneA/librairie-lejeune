640cca1868358125ab5e60a40d22a80386324c3f
A.Lejeune <acelyalejeune@gmail.com>
2024-04-27 23:20:49 +0200

    Update cadeaux.php

--- public/cadeaux.php
+++ public/cadeaux.php
@@ -1,27 +1,37 @@
+<?php
+require_once('C:\xampp\htdocs\librairie-lejeune\admin\settings.php');
+
+// Check if user is not identified, redirect to login page
+if (!isset($_SESSION['IDENTIFY']) || !$_SESSION['IDENTIFY']) {
+    header('Location: login.php');
+    exit();
+}
+
+$msg = null;
+$result = null;
+$execute = false;
+
+// Check the database connection
+if (!is_object($conn)) {
+    $msg = getMessage($conn, 'error');
+} else {
+    // Fetch all livres from the database
+    $result = getAllCadeauxDB($conn);
+
+    // Check if cadeaux exist
+    if (is_array($result) && !empty($result)) {
+        $execute = true;
+    } else {
+        $msg = getMessage('Il n\'y a pas de livre à afficher actuellement', 'error');
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description"
-        content="Découvrez Librairie Lejeune pour des livres, fournitures de papeterie et cadeaux uniques. Parcourez notre sélection dès aujourd'hui!">
-
-
-    <!-- Custom Sass file -->
-    <link rel="stylesheet" href="../css/styles.css">
-
-    <!-- Google Fonts Preconnect and Link -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Chelsea+Market&family=Great+Vibes&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
-
-    <!-- Favicon -->
-
-    <!-- Title -->
-    <title>Cadeaux</title>
+    <?php displayHeadSection('Cadeaux'); ?>
 </head>
 
 <body>
@@ -29,11 +39,11 @@
                                Header
     ------------------------------------------------------------------>
     <header>
-    <!-----------------------------------------------------------------
+        <!-----------------------------------------------------------------
                                Navigation
     ------------------------------------------------------------------>
-    <div data-include="navigation"></div>
-    <!-----------------------------------------------------------------
+        <div data-include="navigation"></div>
+        <!-----------------------------------------------------------------
                             Navigation end
     ------------------------------------------------------------------>
         <div class="header-image--gifts">
@@ -76,191 +86,18 @@
         ------------------------------------------------------------------>
         <section class="gifts-container container">
             <!-- Articles -->
-            <article class="article-container">
-                <!-- Image container -->
-                <div class="product-img">
-                    <img src="../assets/images/gifts/week-end-insolite.jpg" alt="Week-end Insolite">
-                </div>
-                <!--Image container end -->
-                <!-- Info container -->
-                <div class="product-info">
-                    <h2>Bongo FR Week-end Insolite</h2>
-                    <p>Bongo</p>
-
-                    <!-- <div class="product-description">
-                        <p>Cinquante-deux semaines : c'est le temps qu'il reste à Mona pour découvrir toute la beauté du
-                            monde. C'est le temps que s'est donné son grand-père, un... </p>
-                    </div> -->
-
-                    <div class="more-info">
-                        <a href="../public/product.html">Savoir plus</a>
-                    </div>
-                </div>
-                <!-- Info container end -->
-                <!-- Price container -->
-                <div class="product-price">
-                    <p>99,90 € <span><i class="fas fa-truck"></i>Livraison 1 à 2 semaines</span><span><i
-                                class="fas fa-receipt"></i>Retrait en magasin dans 2 h.</span></p>
-
-                    <a href="#" class="btn-primary"><i class="fas fa-shopping-cart"></i>Ajouter au panier</a>
-                </div>
-                <!-- Price container end -->
-            </article>
-
-            <article class="article-container">
-                <!-- Image container -->
-                <div class="product-img">
-                    <img src="../assets/images/gifts/aventure-en-duo.jpg" alt="Aventure en Duo">
-                </div>
-                <!-- Image container end -->
-                <!-- Info container -->
-                <div class="product-info">
-                    <h2>Bongo FR Aventure en Duo</h2>
-                    <p>Bongo</p>
-
-                    <!-- <div class="product-description">
-                        <p>Dans la tranquillité trompeuse de la ville d'Ewing aux États-Unis, Iris, confinée à la
-                            bibliothèque, est plongée dans ses révisions. À des kilomètres ...</p>
-                    </div> -->
-
-                    <div class="more-info">
-                        <a href="../public/product.html">Savoir plus</a>
-                    </div>
-                </div>
-                <!-- Info container end -->
-                <!-- Price container -->
-                <div class="product-price">
-                    <p>39,90 € <span><i class="fas fa-truck"></i>Livraison 1 à 2 semaines</span><span><i
-                                class="fas fa-receipt"></i>Retrait en magasin dans 2 h.</span></p>
-
-                    <a href="#" class="btn-primary"><i class="fas fa-shopping-cart"></i>Ajouter au panier</a>
-                </div>
-                <!-- Price container end -->
-            </article>
-
-            <article class="article-container">
-                <!-- Image container -->
-                <div class="product-img">
-                    <img src="../assets/images/gifts/cinema.jpg" alt="Cinéma & Pop-corn à deux">
-                </div>
-                <!-- Image container end -->
-                <!-- Info container -->
-                <div class="product-info">
-                    <h2>Bongo FR Cinéma & Pop-corn à deux</h2>
-                    <p>Bongo</p>
-
-                    <!-- <div class="product-description">
-                        <p>Les membres du gang des Devil's sons sont plus unis que jamais, mais ils sont confrontés à de
-                            nombreux obstacles. Avalone tente de faire front face à ...</p>
-                    </div> -->
-
-                    <div class="more-info">
-                        <a href="../public/product.html">Savoir plus</a>
-                    </div>
-                </div>
-                <!-- Info container end -->
-                <!-- Price container -->
-                <div class="product-price">
-                    <p>34,90 € <span><i class="fas fa-truck"></i>Livraison 1 à 2 semaines</span><span><i
-                                class="fas fa-receipt"></i>Retrait en magasin dans 2 h.</span></p>
-
-                    <a href="#" class="btn-primary"><i class="fas fa-shopping-cart"></i>Ajouter au panier</a>
-                </div>
-                <!-- Price container end -->
-            </article>
-
-            <article class="article-container">
-                <!-- Image container -->
-                <div class="product-img">
-                    <img src="../assets/images/gifts/moments-de-bien-etre.jpg" alt="Moments de bien-être">
-                </div>
-                <!-- Image container end -->
-                <!-- Info container -->
-                <div class="product-info">
-                    <h2>Bongo FR Moments de bien-être</h2>
-                    <p>Bongo</p>
-
-                    <!-- <div class="product-description">
-                        <p>La rencontre d'Ari Malik, petit-fils d'une suivante de la princesse Indira, et de Rebecca
-                            Bradley, jeune actrice américaine, dans les ruines d'un mano...</p>
-                    </div> -->
-
-                    <div class="more-info">
-                        <a href="../public/product.html">Savoir plus</a>
-                    </div>
-                </div>
-                <!-- Info container end -->
-                <!-- Price container -->
-                <div class="product-price">
-                    <p>49,90 € <span><i class="fas fa-truck"></i>Livraison 1 à 2 semaines</span><span><i
-                                class="fas fa-receipt"></i>Retrait en magasin dans 2 h.</span></p>
-
-                    <a href="#" class="btn-primary"><i class="fas fa-shopping-cart"></i>Ajouter au panier</a>
-                </div>
-                <!-- Price container end -->
-            </article>
-
-            <article class="article-container">
-                <!-- Image container -->
-                <div class="product-img">
-                    <img src="../assets/images/gifts/joyeux-anniversaire.jpg" alt="Joyeux Anniversaire">
-                </div>
-                <!-- Image container end -->
-                <!-- Info container -->
-                <div class="product-info">
-                    <h2>Bongo FR Joyeux Anniversaire</h2>
-                    <p>Bongo</p>
-
-                    <!-- <div class="product-description">
-                        <p>Répondant à une annonce pour un travail de femme de ménage, Millie, qui sort tout juste de
-                            dix ans de prison, devient domestique chez les Winchester,</p>
-                    </div> -->
-
-                    <div class="more-info">
-                        <a href="../public/product.html">Savoir plus</a>
-                    </div>
-                </div>
-                <!-- Info container end -->
-                <!-- Price container -->
-                <div class="product-price">
-                    <p>49,90 € <span><i class="fas fa-truck"></i>Livraison 1 à 2 semaines</span><span><i
-                                class="fas fa-receipt"></i>Retrait en magasin dans 2 h.</span></p>
-
-                    <a href="#" class="btn-primary"><i class="fas fa-shopping-cart"></i>Ajouter au panier</a>
-                </div>
-                <!-- Price container end -->
-            </article>
-
-            <article class="article-container">
-                <!-- Image container -->
-                <div class="product-img">
-                    <img src="../assets/images/gifts/tables-romantiques.jpg" alt="Tables Romantiques">
-                </div>
-                <!-- Image container end -->
-                <!-- Info container -->
-                <div class="product-info">
-                    <h2>Bongo FR Tables Romantiques</h2>
-                    <p>Bongo</p>
-
-                    <!-- <div class="product-description">
-                        <p>Etudiante en médecine, la narratrice intègre la prestigieuse université John Hopkins.
-                            Lorsqu'elle rencontre Ezrah Milton, elle comprend qu'il l'a élue...</p>
-                    </div> -->
-
-                    <div class="more-info">
-                        <a href="../public/product.html">Savoir plus</a>
-                    </div>
-                </div>
-                <!-- Info container end -->
-                <!-- Price container -->
-                <div class="product-price">
-                    <p>69,90 € <span><i class="fas fa-truck"></i>Livraison 1 à 2 semaines</span><span><i
-                                class="fas fa-receipt"></i>Retrait en magasin dans 2 h.</span></p>
-
-                    <a href="#" class="btn-primary"><i class="fas fa-shopping-cart"></i>Ajouter au panier</a>
-                </div>
-                <!-- Price container end -->
-            </article>
+            <?php
+            // Check if there are livres to display
+            if ($execute) {
+                // Loop through each livre and generate HTML markup
+                foreach ($result as $livre) {
+                    echo generateCadeauHTML($livre);
+                }
+            } else {
+                // Display a message if there are no livres to display
+                echo '<p>Il n\'y a pas de livre à afficher actuellement</p>';
+            }
+            ?>
             <!-- Articles end -->
 
             <!-- Pagination -->
