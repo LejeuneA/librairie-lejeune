8aea66cef98d57113bfa5109a3748413a9802e89
A.Lejeune <acelyalejeune@gmail.com>
2024-04-28 13:04:16 +0200

    Update product-livre.php

--- public/product-livre.php
+++ public/product-livre.php
@@ -64,8 +64,11 @@ if (isset($_GET['idLivre']) && !empty($_GET['idLivre'])) {
     <footer>
         <div data-include="footer"></div>
     </footer>
+
+
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     <!-- Include functions.js -->
     <script src="../js/functions.js"></script>
 </body>
