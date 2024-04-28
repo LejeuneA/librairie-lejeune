3cb596de643598090dc868c16b73e2bb0cb9c0b4
A.Lejeune <acelyalejeune@gmail.com>
2024-04-28 13:04:30 +0200

    Update product-cadeau.php

--- public/product-cadeau.php
+++ public/product-cadeau.php
@@ -64,8 +64,10 @@ if (isset($_GET['idCadeau']) && !empty($_GET['idCadeau'])) {
     <footer>
         <div data-include="footer"></div>
     </footer>
+
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     <!-- Include functions.js -->
     <script src="../js/functions.js"></script>
 </body>
