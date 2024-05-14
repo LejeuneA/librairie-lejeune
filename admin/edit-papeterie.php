8317021f9e8758932473e03051d865878c651b15
A.Lejeune <acelyalejeune@gmail.com>
2024-05-14 15:47:28 +0200

    Update edit-papeterie.php

--- admin/edit-papeterie.php
+++ admin/edit-papeterie.php
@@ -169,11 +169,6 @@ if (isset($_SESSION['form_submitted'])) {
 
                     <!-- Form right -->
                     <div class="form-right">
-                        <!-- URL of the image -->
-                        <div class="form-ctrl">
-                            <label for="image_url" class="form-ctrl">URL de l'image</label>
-                            <input type="text" class="form-ctrl" id="image_url" name="image_url" value="<?php echo isset($papeterie['image_url']) ? $papeterie['image_url'] : ''; ?>" readonly>
-                        </div>
 
                         <!-- File upload field -->
                         <div class="form-ctrl">
@@ -183,6 +178,7 @@ if (isset($_SESSION['form_submitted'])) {
                         <!-- Preview of the image -->
                         <div class="form-ctrl">
                             <label for="image_preview" class="form-ctrl">Aperçu de l'image</label>
+                            <p><span>URL de l'image:</span> <?php echo isset($papeterie['image_url']) ? $papeterie['image_url'] : ''; ?></p>
                             <div>
                                 <img id="image_preview" class="image_preview" src="<?php echo isset($papeterie['image_url']) ? $papeterie['image_url'] : ''; ?>" alt="Aperçu de l'image">
                             </div>
