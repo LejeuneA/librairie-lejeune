f4c697b1f8739f98a54d29f801c3b0492a47ca39
A.Lejeune <acelyalejeune@gmail.com>
2024-05-30 14:41:04 +0200

    Update work-with-us.php

--- public/work-with-us.php
+++ public/work-with-us.php
@@ -8,8 +8,7 @@ require_once('../admin/settings.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description"
-        content="Découvrez Librairie Lejeune pour des livres, fournitures de papeterie et cadeaux uniques. Parcourez notre sélection dès aujourd'hui!">
+    <meta name="description" content="Découvrez Librairie Lejeune pour des livres, fournitures de papeterie et cadeaux uniques. Parcourez notre sélection dès aujourd'hui!">
 
 
     <!-- Custom Sass file -->
@@ -18,9 +17,7 @@ require_once('../admin/settings.php');
     <!-- Google Fonts Preconnect and Link -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Chelsea+Market&family=Great+Vibes&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Chelsea+Market&family=Great+Vibes&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <!-- Favicon -->
 
@@ -29,156 +26,176 @@ require_once('../admin/settings.php');
 </head>
 
 <body>
-   <!-----------------------------------------------------------------
+    <!-----------------------------------------------------------------
                                Header
     ------------------------------------------------------------------>
     <header>
         <!-----------------------------------------------------------------
                                Navigation
     	------------------------------------------------------------------>
-		<nav class="navbar">
-			<div class="navbar-container container">
-				<!-- Logo -->
-				<a href="../index.php">
-					<img src="../assets/logo/librairie-lejeune.png" class="navbar-brand-img" alt="Librairie Lejeune Logo">
-				</a>
-				<!-- Logo end -->
+        <nav class="navbar">
+            <div class="navbar-container container">
+                <!-- Logo -->
+                <a href="../index.php">
+                    <img src="../assets/logo/librairie-lejeune.png" class="navbar-brand-img" alt="Librairie Lejeune Logo">
+                </a>
+                <!-- Logo end -->
 
-				<!-- Right-side content -->
-				<div class="d-flex">
-					<!-- Social icons -->
-					<ul class="social-nav">
-						<!-- Icons -->
-						<li class="social-item">
-							<a class="social-link" href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-								<i class="fa-brands fa-square-facebook fa-lg"></i>
-							</a>
-						</li>
-						<li class="social-item">
-							<a class="social-link" href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
-								<i class="fa-brands fa-x-twitter fa-lg"></i>
-							</a>
-						</li>
-						<li class="social-item">
-							<a class="social-link" href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-								<i class="fa-brands fa-instagram fa-lg"></i>
-							</a>
-						</li>
-					</ul>
-					<!-- Social icons end -->
+                <!-- Right-side content -->
+                <div class="d-flex">
+                    <!-- Social icons -->
+                    <ul class="social-nav">
+                        <!-- Icons -->
+                        <li class="social-item">
+                            <a class="social-link" href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                                <i class="fa-brands fa-square-facebook fa-lg"></i>
+                            </a>
+                        </li>
+                        <li class="social-item">
+                            <a class="social-link" href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+                                <i class="fa-brands fa-x-twitter fa-lg"></i>
+                            </a>
+                        </li>
+                        <li class="social-item">
+                            <a class="social-link" href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                                <i class="fa-brands fa-instagram fa-lg"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- Social icons end -->
 
-					<!-- Search -->
-					<form class="search" role="search" action="./admin/search.php" method="GET">
-						<div class="search-group">
-							<input class="form-control" type="search" name="query" placeholder="Que cherchez-vous?" aria-label="Search">
-							<button class="btn-search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-						</div>
-					</form>
-					<!-- Search end -->
+                    <!-- Search -->
+                    <form class="search" role="search" action="./admin/search.php" method="GET">
+                        <div class="search-group">
+                            <input class="form-control" type="search" name="query" placeholder="Que cherchez-vous?" aria-label="Search">
+                            <button class="btn-search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                        </div>
+                    </form>
+                    <!-- Search end -->
 
-					<!-- Customer button -->
-					<a href="http://localhost/librairie-lejeune/admin/customer.php" class="btn-customer"><i class="fa-solid fa-user"></i> Mon compte</a>
-					<!-- Customer button -->
+                    <!-- Customer button -->
+                    <?php
+                    if (!isset($_SESSION['IDENTIFY']) || !$_SESSION['IDENTIFY']) {
+                        echo '<a href="http://localhost/librairie-lejeune/admin/login.php" class="btn-primary">Se connecter</a>';
+                    } elseif (isset($_SESSION['user_permission'])) {
+                        if ($_SESSION['user_permission'] == 1) {
+                            echo '<a href="http://localhost/librairie-lejeune/admin/manager.php" class="btn-customer"><i class="fa-solid fa-user"></i> Mon compte</a>';
+                        } elseif ($_SESSION['user_permission'] == 2) {
+                            echo '<a href="http://localhost/librairie-lejeune/admin/customer.php" class="btn-customer"><i class="fa-solid fa-user"></i> Mon compte</a>';
+                        }
+                    }
+                    ?>
+                    <!-- Customer button end -->
 
-					<!-- Login button -->
-					<?php if (!isset($_SESSION['IDENTIFY']) || !$_SESSION['IDENTIFY']) : ?>
-						<a href="http://localhost/librairie-lejeune/admin/login.php" class="btn-primary">Se connecter</a>
-					<?php else : ?>
-						<a href="http://localhost/librairie-lejeune/admin/logoff.php" class="btn-primary">Déconnexion</a>
-					<?php endif; ?>
-					<!-- Login button end -->
+                    <!-- Login button -->
+                    <?php if (!isset($_SESSION['IDENTIFY']) || !$_SESSION['IDENTIFY']) : ?>
+                        <a href="http://localhost/librairie-lejeune/admin/login.php" class="btn-primary">Se connecter</a>
+                    <?php else : ?>
+                        <a href="http://localhost/librairie-lejeune/admin/logoff.php" class="btn-primary">Déconnexion</a>
+                    <?php endif; ?>
+                    <!-- Login button end -->
 
 
-				</div>
-				<!-- Right-side content end -->
-			</div>
-		</nav>
+                </div>
+                <!-- Right-side content end -->
+            </div>
+        </nav>
 
-		<!---------------------------------------------------------------
+        <!---------------------------------------------------------------
                                 Menu
     	----------------------------------------------------------------->
-		<div class="navbar-menu">
-			<ul class="navbar-nav">
-				<li class="nav-item">
-					<a class="nav-link" href="../index.php">Accueil</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="../public/livres.php">Livres</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="../public/papeteries.php">Papeterie</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="../public/cadeaux.php">Cadeaux</a>
-				</li>
-			</ul>
-		</div>
-		<!---------------------------------------------------------------
+        <div class="navbar-menu">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="../index.php">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../public/livres.php">Livres</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../public/papeteries.php">Papeterie</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../public/cadeaux.php">Cadeaux</a>
+                </li>
+            </ul>
+        </div>
+        <!---------------------------------------------------------------
                          Menu end
     	---------------------------------------------------------------->
 
-		<!---------------------------------------------------------------
+        <!---------------------------------------------------------------
                              Offcanvas menu
     	----------------------------------------------------------------->
-		<div id="mySidenav" class="sidenav">
+        <div id="mySidenav" class="sidenav">
 
-			<!-- Search -->
-			<form class="search" role="search" action="./admin/search.php" method="GET">
-				<div class="search-group">
-					<input class="form-control" type="search" name="query" placeholder="Que cherchez-vous?" aria-label="Search">
-					<button class="btn-search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-				</div>
-			</form>
-			<!-- Search end -->
+            <!-- Search -->
+            <form class="search" role="search" action="./admin/search.php" method="GET">
+                <div class="search-group">
+                    <input class="form-control" type="search" name="query" placeholder="Que cherchez-vous?" aria-label="Search">
+                    <button class="btn-search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+            </form>
+            <!-- Search end -->
 
-			<!-- Menu -->
-			<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-			<a class="nav-link" href="../index.php">Accueil</a>
-			<a class="nav-link" href="../public/livres.php">Livres</a>
-			<a class="nav-link" href="../public/papeteries.php">Papeterie</a>
-			<a class="nav-link" href="../public/cadeaux.php">Cadeaux</a>
-			<!-- Menu end -->
+            <!-- Menu -->
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a class="nav-link" href="../index.php">Accueil</a>
+            <a class="nav-link" href="../public/livres.php">Livres</a>
+            <a class="nav-link" href="../public/papeteries.php">Papeterie</a>
+            <a class="nav-link" href="../public/cadeaux.php">Cadeaux</a>
+            <!-- Menu end -->
 
-			<!-- Customer button -->
-			<a href="http://localhost/librairie-lejeune/admin/customer.php" class="btn-customer"><i class="fa-solid fa-user"></i> Mon compte</a>
-			<!-- Customer button -->
+            <!-- Customer button -->
+            <?php
+            if (!isset($_SESSION['IDENTIFY']) || !$_SESSION['IDENTIFY']) {
+                echo '<a href="http://localhost/librairie-lejeune/admin/login.php" class="btn-primary">Se connecter</a>';
+            } elseif (isset($_SESSION['user_permission'])) {
+                if ($_SESSION['user_permission'] == 1) {
+                    echo '<a href="http://localhost/librairie-lejeune/admin/manager.php" class="btn-customer"><i class="fa-solid fa-user"></i> Mon compte</a>';
+                } elseif ($_SESSION['user_permission'] == 2) {
+                    echo '<a href="http://localhost/librairie-lejeune/admin/customer.php" class="btn-customer"><i class="fa-solid fa-user"></i> Mon compte</a>';
+                }
+            }
+            ?>
+            <!-- Customer button end -->
 
-			<!-- Login button -->
-			<?php if (!isset($_SESSION['IDENTIFY']) || !$_SESSION['IDENTIFY']) : ?>
-				<a href="http://localhost/librairie-lejeune/admin/login.php" class="btn-login">Se connecter</a>
-			<?php else : ?>
-				<a href="http://localhost/librairie-lejeune/admin/logoff.php" class="btn-login">Déconnexion</a>
-			<?php endif; ?>
-			<!-- Login button end -->
+            <!-- Login button -->
+            <?php if (!isset($_SESSION['IDENTIFY']) || !$_SESSION['IDENTIFY']) : ?>
+                <a href="http://localhost/librairie-lejeune/admin/login.php" class="btn-login">Se connecter</a>
+            <?php else : ?>
+                <a href="http://localhost/librairie-lejeune/admin/logoff.php" class="btn-login">Déconnexion</a>
+            <?php endif; ?>
+            <!-- Login button end -->
 
-			<!-- Social icons -->
-			<ul class="social-nav">
-				<!-- Icons -->
-				<li class="social-item">
-					<a class="social-link" href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-						<i class="fa-brands fa-square-facebook fa-lg"></i>
-					</a>
-				</li>
-				<li class="social-item">
-					<a class="social-link" href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
-						<i class="fa-brands fa-x-twitter fa-lg"></i>
-					</a>
-				</li>
-				<li class="social-item">
-					<a class="social-link" href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-						<i class="fa-brands fa-instagram fa-lg"></i>
-					</a>
-				</li>
-			</ul>
-			<!-- Social icons end -->
+            <!-- Social icons -->
+            <ul class="social-nav">
+                <!-- Icons -->
+                <li class="social-item">
+                    <a class="social-link" href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                        <i class="fa-brands fa-square-facebook fa-lg"></i>
+                    </a>
+                </li>
+                <li class="social-item">
+                    <a class="social-link" href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+                        <i class="fa-brands fa-x-twitter fa-lg"></i>
+                    </a>
+                </li>
+                <li class="social-item">
+                    <a class="social-link" href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                        <i class="fa-brands fa-instagram fa-lg"></i>
+                    </a>
+                </li>
+            </ul>
+            <!-- Social icons end -->
 
-		</div>
+        </div>
 
-		<!-- Hamburger icon for smaller screens -->
-		<div class="navbar-hamburger">
-			<div id="hamburger" onclick="openNav()"><i class="fa-solid fa-bars"></i></div>
-		</div>
-		<!------------------------------------------------------------- 
+        <!-- Hamburger icon for smaller screens -->
+        <div class="navbar-hamburger">
+            <div id="hamburger" onclick="openNav()"><i class="fa-solid fa-bars"></i></div>
+        </div>
+        <!------------------------------------------------------------- 
                           Offcanvas menu end
     	--------------------------------------------------------------->
         <div class="header-image--work-with-us">
@@ -279,8 +296,7 @@ require_once('../admin/settings.php');
                 <ul>
                     <li>
                         <i class="fa-regular fa-envelope"></i>
-                        Envoyez-nous votre CV par mail <br><a
-                            href="mailto:contact@acelyalejeune.be">contact@librairie.com</a>
+                        Envoyez-nous votre CV par mail <br><a href="mailto:contact@acelyalejeune.be">contact@librairie.com</a>
                     </li>
                 </ul>
             </div>
@@ -306,9 +322,7 @@ require_once('../admin/settings.php');
     ------------------------------------------------------------------>
 
     <!-- Font Awesome -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js"
-        integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- Include functions.js -->
     <script src="../js/functions.js"></script>
