f3a23a1bfebb732f9472fe9ab7657a5c9261268e
A.Lejeune <acelyalejeune@gmail.com>
2024-05-30 14:41:02 +0200

    Update product-papeterie.php

--- public/product-papeterie.php
+++ public/product-papeterie.php
@@ -7,22 +7,22 @@ $execute = false;
 
 // Check if the ID of the papeterie is passed in the URL
 if (isset($_GET['idPapeterie']) && !empty($_GET['idPapeterie'])) {
-    $idPapeterie = $_GET['idPapeterie'];
-    // Ensure that the database connection object is valid
-    if (!is_object($conn)) {
-        $msg = getMessage($conn, 'error');
-    } else {
-        // Fetch the papeterie from the database based on the ID
-        $result = getPapeterieByIDDB($conn, $idPapeterie);
-        // Check if the result is a valid array and not empty
-        if (isset($result) && is_array($result) && !empty($result)) {
-            $execute = true;
-        } else {
-            $msg = getMessage('Il n\'y a pas du produit à afficher', 'error');
-        }
-    }
+	$idPapeterie = $_GET['idPapeterie'];
+	// Ensure that the database connection object is valid
+	if (!is_object($conn)) {
+		$msg = getMessage($conn, 'error');
+	} else {
+		// Fetch the papeterie from the database based on the ID
+		$result = getPapeterieByIDDB($conn, $idPapeterie);
+		// Check if the result is a valid array and not empty
+		if (isset($result) && is_array($result) && !empty($result)) {
+			$execute = true;
+		} else {
+			$msg = getMessage('Il n\'y a pas du produit à afficher', 'error');
+		}
+	}
 } else {
-    $msg = getMessage('Il n\'y a pas du produit à afficher', 'error');
+	$msg = getMessage('Il n\'y a pas du produit à afficher', 'error');
 }
 ?>
 
@@ -31,15 +31,15 @@ if (isset($_GET['idPapeterie']) && !empty($_GET['idPapeterie'])) {
 <html lang="fr">
 
 <head>
-    <?php displayHeadSection('Papeterie'); ?>
+	<?php displayHeadSection('Papeterie'); ?>
 </head>
 
 <body>
-    <!-----------------------------------------------------------------
+	<!-----------------------------------------------------------------
                                Header
     ------------------------------------------------------------------>
-    <header>
-       <!-----------------------------------------------------------------
+	<header>
+		<!-----------------------------------------------------------------
                                Navigation
     	------------------------------------------------------------------>
 		<nav class="navbar">
@@ -83,8 +83,18 @@ if (isset($_GET['idPapeterie']) && !empty($_GET['idPapeterie'])) {
 					<!-- Search end -->
 
 					<!-- Customer button -->
-					<a href="http://localhost/librairie-lejeune/admin/customer.php" class="btn-customer"><i class="fa-solid fa-user"></i> Mon compte</a>
-					<!-- Customer button -->
+					<?php
+					if (!isset($_SESSION['IDENTIFY']) || !$_SESSION['IDENTIFY']) {
+						echo '<a href="http://localhost/librairie-lejeune/admin/login.php" class="btn-primary">Se connecter</a>';
+					} elseif (isset($_SESSION['user_permission'])) {
+						if ($_SESSION['user_permission'] == 1) {
+							echo '<a href="http://localhost/librairie-lejeune/admin/manager.php" class="btn-customer"><i class="fa-solid fa-user"></i> Mon compte</a>';
+						} elseif ($_SESSION['user_permission'] == 2) {
+							echo '<a href="http://localhost/librairie-lejeune/admin/customer.php" class="btn-customer"><i class="fa-solid fa-user"></i> Mon compte</a>';
+						}
+					}
+					?>
+					<!-- Customer button end -->
 
 					<!-- Login button -->
 					<?php if (!isset($_SESSION['IDENTIFY']) || !$_SESSION['IDENTIFY']) : ?>
@@ -146,8 +156,18 @@ if (isset($_GET['idPapeterie']) && !empty($_GET['idPapeterie'])) {
 			<!-- Menu end -->
 
 			<!-- Customer button -->
-			<a href="http://localhost/librairie-lejeune/admin/customer.php" class="btn-customer"><i class="fa-solid fa-user"></i> Mon compte</a>
-			<!-- Customer button -->
+			<?php
+			if (!isset($_SESSION['IDENTIFY']) || !$_SESSION['IDENTIFY']) {
+				echo '<a href="http://localhost/librairie-lejeune/admin/login.php" class="btn-primary">Se connecter</a>';
+			} elseif (isset($_SESSION['user_permission'])) {
+				if ($_SESSION['user_permission'] == 1) {
+					echo '<a href="http://localhost/librairie-lejeune/admin/manager.php" class="btn-customer"><i class="fa-solid fa-user"></i> Mon compte</a>';
+				} elseif ($_SESSION['user_permission'] == 2) {
+					echo '<a href="http://localhost/librairie-lejeune/admin/customer.php" class="btn-customer"><i class="fa-solid fa-user"></i> Mon compte</a>';
+				}
+			}
+			?>
+			<!-- Customer button end -->
 
 			<!-- Login button -->
 			<?php if (!isset($_SESSION['IDENTIFY']) || !$_SESSION['IDENTIFY']) : ?>
@@ -187,36 +207,36 @@ if (isset($_GET['idPapeterie']) && !empty($_GET['idPapeterie'])) {
 		<!------------------------------------------------------------- 
                           Offcanvas menu end
     	--------------------------------------------------------------->
-    </header>
-    <!-----------------------------------------------------------------
+	</header>
+	<!-----------------------------------------------------------------
                                Header end
     ------------------------------------------------------------------>
-    <!-- Main -->
-    <main>
-        <div class="container">
-            <div id="message">
-                <?php if (isset($msg)) echo $msg; ?>
-            </div>
-            <div id="content">
-                <?php
-                // Peut-on exécuter l'affichage de l'article
-                if ($execute) {
-                    displayPapeterieByID($result);
-                }
-                ?>
-            </div>
-        </div>
-    </main>
-    <!-- Footer -->
-    <footer>
-        <div data-include="footer"></div>
-    </footer>
+	<!-- Main -->
+	<main>
+		<div class="container">
+			<div id="message">
+				<?php if (isset($msg)) echo $msg; ?>
+			</div>
+			<div id="content">
+				<?php
+				// Peut-on exécuter l'affichage de l'article
+				if ($execute) {
+					displayPapeterieByID($result);
+				}
+				?>
+			</div>
+		</div>
+	</main>
+	<!-- Footer -->
+	<footer>
+		<div data-include="footer"></div>
+	</footer>
 
-    <!-- Font Awesome -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<!-- Font Awesome -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <!-- Include functions.js -->
-    <script src="../js/functions.js"></script>
+	<!-- Include functions.js -->
+	<script src="../js/functions.js"></script>
 
 </body>
 
