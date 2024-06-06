46b51e727ba483cbe0002ead2bfa19710b35fb6c
A.Lejeune <acelyalejeune@gmail.com>
2024-06-06 11:47:32 +0200

    Update edit-livre.php

--- admin/edit-livre.php
+++ admin/edit-livre.php
@@ -212,9 +212,6 @@ if (isset($_SESSION['form_submitted'])) {
         </div>
     </div>
 
-    <?php
-    displayJSSection($tinyMCE);
-    ?>
     <!-- Footer -->
     <footer>
         <div data-include="footer"></div>
@@ -223,6 +220,9 @@ if (isset($_SESSION['form_submitted'])) {
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+    <?php
+    displayJSSection($tinyMCE);
+    ?>
     <!-- Functions -->
     <script src="../js/functions.js"></script>
 
