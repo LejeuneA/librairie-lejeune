3c34ceae19931b0dbe86e0479dddabf78c87c857
A.Lejeune <acelyalejeune@gmail.com>
2024-06-25 09:40:27 +0200

    Update livres.php

--- public/livres.php
+++ public/livres.php
@@ -75,7 +75,7 @@ if (!is_object($conn)) {
 					<!-- Social icons end -->
 
 					<!-- Search -->
-					<form class="search" role="search" action="./admin/search.php" method="GET">
+					<form class="search" role="search" action="../admin/search.php" method="GET">
 						<div class="search-group">
 							<input class="form-control" type="search" name="query" placeholder="Que cherchez-vous?" aria-label="Search">
 							<button class="btn-search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
