2f9299b46051617bbcd4f9d93ff5b32ddd424a9f
A.Lejeune <acelyalejeune@gmail.com>
2024-06-25 09:40:37 +0200

    Update work-with-us.php

--- public/work-with-us.php
+++ public/work-with-us.php
@@ -138,7 +138,7 @@ require_once('../admin/settings.php');
         <div id="mySidenav" class="sidenav">
 
             <!-- Search -->
-            <form class="search" role="search" action="./admin/search.php" method="GET">
+            <form class="search" role="search" action="../admin/search.php" method="GET">
                 <div class="search-group">
                     <input class="form-control" type="search" name="query" placeholder="Que cherchez-vous?" aria-label="Search">
                     <button class="btn-search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
